d97748da098e94aea0c2b8633f0ab0ac9d33cb42
Lantos <me@lantos.in>
2024-03-10 22:26:00 +0100

    Update btorrent.sh

--- btorrent.sh
+++ btorrent.sh
@@ -4,23 +4,24 @@
 # Credit to original author: sam https://github.com/nikzad-avasam/block-torrent-on-server
 # GitHub:   https://github.com/nikzad-avasam/block-torrent-on-server
 # Author:   sam nikzad
+# Modify: LantosBro (https://github.com/lantosbro)
 
-echo -n "Blocking all torrent trafic on your server. pls wait ... "
-wget -q -O/etc/trackers https://raw.githubusercontent.com/nikzad-avasam/block-torrent-on-server/main/domains
+echo -n "Blocking all torrent traffic on your server. Please wait... "
+wget -q -O/etc/trackers https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/domains
 cat >/etc/cron.daily/denypublic<<'EOF'
 IFS=$'\n'
 L=$(/usr/bin/sort /etc/trackers | /usr/bin/uniq)
 for fn in $L; do
-        /sbin/iptables -D INPUT -d $fn -j DROP
-        /sbin/iptables -D FORWARD -d $fn -j DROP
-        /sbin/iptables -D OUTPUT -d $fn -j DROP
-        /sbin/iptables -A INPUT -d $fn -j DROP
-        /sbin/iptables -A FORWARD -d $fn -j DROP
-        /sbin/iptables -A OUTPUT -d $fn -j DROP
+        ufw delete deny out to $fn
+        ufw delete deny in to $fn
+        ufw delete deny forward to $fn
+        ufw deny out to $fn
+        ufw deny in to $fn
+        ufw deny forward to $fn
 done
 EOF
 chmod +x /etc/cron.daily/denypublic
-curl -s -LO https://raw.githubusercontent.com/nikzad-avasam/block-torrent-on-server/main/Thosts
+curl -s -LO https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/Thosts
 cat Thosts >> /etc/hosts
 sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
-echo "${OK}"
+echo "Done."
