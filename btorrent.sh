4fc72da61e2e0a56035d58b5f430b5098e73174a
Lantos <me@lantos.in>
2024-03-11 04:38:26 +0100

    Update btorrent.sh

--- btorrent.sh
+++ btorrent.sh
@@ -13,12 +13,22 @@ for cmd in dig awk ufw; do
     fi
 done
 
+config_file="/etc/block-torrent-on-server.conf"
+
+if [ "$1" == "--no-hosts" ]; then
+    echo "UPDATE_HOSTS=0" > $config_file
+else
+    echo "UPDATE_HOSTS=1" > $config_file
+fi
+
 echo -n "Blocking all torrent traffic on your server. Please wait... "
 wget -q -O/etc/trackers https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/domains
 wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk -F'[:/]' '{print $4}' | sort -u >> /etc/trackers
 wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt | sed -n 's/^.*:\/\/\([^:]*\).*$/\1/p' | sort -u > /etc/tracker_ips
 cat >/etc/cron.daily/denypublic<<'EOF'
 #!/bin/bash
+source /etc/block-torrent-on-server.conf
+
 rm /etc/trackers
 rm /etc/tracker_ips
 wget -q -O/etc/trackers https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/domains
@@ -39,8 +49,10 @@ for fn in $L; do
         for IP in $IPs; do
                 /usr/sbin/ufw delete deny out to $IP
                 /usr/sbin/ufw delete deny in from $IP
+                /usr/sbin/ufw delete deny forward to $IP
                 /usr/sbin/ufw deny out to $IP
                 /usr/sbin/ufw deny in from $IP
+                /usr/sbin/ufw deny forward to $IP
         done
 done
 
@@ -54,14 +66,22 @@ for IP in $L; do
         /usr/sbin/ufw deny forward to $IP
 done
 
-curl -s -LO https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/Thosts
-cat Thosts >> /etc/hosts
-wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk -F'[:/]' '{print "0.0.0.0", $4}' >> /etc/hosts
-sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
+if [ "$UPDATE_HOSTS" == "1" ]; then
+    curl -s -LO https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/Thosts
+    cat Thosts >> /etc/hosts
+    wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk -F'[:/]' '{print "0.0.0.0", $4}' >> /etc/hosts
+    sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
+fi
 EOF
 chmod +x /etc/cron.daily/denypublic
-curl -s -LO https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/Thosts
-cat Thosts >> /etc/hosts
-wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk -F'[:/]' '{print "0.0.0.0", $4}' >> /etc/hosts
-sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
+
+source $config_file
+
+if [ "$UPDATE_HOSTS" == "1" ]; then
+    curl -s -LO https://raw.githubusercontent.com/LantosBro/block-torrent-on-server/main/Thosts
+    cat Thosts >> /etc/hosts
+    wget -q -O- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk -F'[:/]' '{print "0.0.0.0", $4}' >> /etc/hosts
+    sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
+fi
+
 echo "Done."
